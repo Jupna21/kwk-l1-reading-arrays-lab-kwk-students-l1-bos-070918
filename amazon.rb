2b347933da980cc0f2d025714d6a193cbd2b8cba
Jupna21 <jupna21@yahoo.com>
2018-07-11 17:15:24 +0000

    automatically backed up by learn

---
+++ amazon.rb
@@ -0,0 +1,11 @@
+def cart_items_prices_stax
+  cart_items_prices = [12.43, 19.99, 3.49, 75.00]
+  
+  cart_items_prices_stax= []
+  
+  cart_items_prices.each do |cart_items_prices|
+  cart_items_prices_stax = cart_items_prices * 1.07
+ puts cart_items_prices_stax
+end
+
+puts cart_items_prices_stax
